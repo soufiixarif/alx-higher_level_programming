e9928ca99d510919d9236a23506c203dbb69457f
soufiane <soufiixarif@github.com>
2024-03-12 19:30:03 +0000

    linkedlist

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *node;
+    listint_t *tmp;
+    listint_t *ptr;
+
+    node = malloc(sizeof (listint_t));
+    if(!node)
+        return (NULL);
+    node->n = number;
+    node->next = NULL;
+    tmp = *head;
+    ptr = *head;
+    while (tmp)
+    {
+        if (node->n < tmp->n)
+        {
+            ptr->next = node;
+            node->next = tmp;
+            return(node);
+        }
+        ptr = tmp;
+        tmp = tmp->next;
+    }
+    return (NULL);
+}
