434aba9ca2280df093325b47130841a63e23964a
soufiane <soufiixarif@github.com>
2024-03-15 03:17:27 +0000

    100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, div, mul
+    import sys
+    c = len(sys.argv)
+    if c != 4:
+        print("sage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operation = sys.argv[2]
+    if operation == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        sys.exit(0)
+    elif operation == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        sys.exit(0)
+    elif operation == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        sys.exit(0)
+    elif operation == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+        sys.exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
